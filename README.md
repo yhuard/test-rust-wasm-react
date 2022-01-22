62c39dcfdba99c4a78d6ec13730408ebcf596745
Yannick Huard <huard.yan@gmail.com>
2022-01-22 18:05:29 +0100

    Setup repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# From Rust to WebAssembly, in a React app
+
+Based on this article: https://www.joshfinnie.com/blog/using-webassembly-created-in-rust-for-fast-react-components/
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[hash].js",
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    hot: true,
+    static: "./dist",
+    historyApiFallback: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  mode: "development",
+  devtool: "inline-source-map",
+};
